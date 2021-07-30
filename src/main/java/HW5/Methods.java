997a4087eaf183fc9dee3a3da49e05affd74b041
PepegaProgrammer <kirillu-vlg@mail.ru>
2021-07-30 16:49:00 +0300

    HomeWork5.

--- src/main/java/HW5/Methods.java
+++ src/main/java/HW5/Methods.java
@@ -45,7 +45,7 @@ public class Methods extends Thread {
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < a2.length; i++) {
+                for (int i = h; i < a2.length; i++) {
                     a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
             }
