dc14916f6074328fbbeeea1c556d9f2dce7a5069
PepegaProgrammer <kirillu-vlg@mail.ru>
2021-07-30 16:52:00 +0300

    HomeWork5. Многопоточность.

--- src/main/java/HW5/Methodds.java
+++ src/main/java/HW5/Methodds.java
@@ -2,30 +2,30 @@ package HW5;
 
 import java.util.Arrays;
 
-public class Methods extends Thread {
-     static final int size = 100000;
-     static final int h = size / 2;
-     static float[] arr = new float[size];
-     static float[] arr1 = new float[size];
+public class Methodds extends Thread {
+    static final int size = 100000;
+    static final int h = size / 2;
+    static float[] arr = new float[size];
+    static float[] arr1 = new float[size];
 
 
     public static void main(String[] args) {
-        Methods method = new Methods();
+        Methodds method = new Methodds();
         method.full();
         method.full1();
         System.out.println(Arrays.equals(arr, arr1));
     }
-     public static void full() {
+    public static void full() {
         Arrays.fill(arr, 1);
         long a = System.currentTimeMillis();
-         for (int i = 0; i < arr.length; i++) {
-             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-         }
-         System.out.println("Время выполнения  метода 1 = " +(System.currentTimeMillis() - a));
-     }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println("Время выполнения  метода 1 = " +(System.currentTimeMillis() - a));
+    }
 
 
-     public static void full1() {
+    public static void full1() {
         float[] a1 = new float[h];
         float[] a2 = new float[h];
         Arrays.fill(arr1, 1);
@@ -54,10 +54,10 @@ public class Methods extends Thread {
         t2.start();
 
 
-         System.arraycopy(a1, 0, arr1, 0, h);
+        System.arraycopy(a1, 0, arr1, 0, h);
         System.arraycopy(a2, 0, arr1, h, h);
 
         System.out.println("Время выполнения всего метода 2 = " + (System.currentTimeMillis() - a));
-     }
+    }
 
 }
